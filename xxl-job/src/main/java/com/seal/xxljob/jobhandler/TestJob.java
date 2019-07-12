3e259101875006174aa096ad4cc09ef0d6f718ca
youngMen1 <fengzhiqiangzxx199644>
2019-07-12 18:09:53 +0800

    Initial commit

---
+++ xxl-job/src/main/java/com/seal/xxljob/jobhandler/TestJob.java
@@ -0,0 +1,23 @@
+package com.seal.xxljob.jobhandler;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/7/12 18:03
+ * @description
+ **/
+@Slf4j
+@Component
+@JobHandler(value = "TestJob")
+public class TestJob extends IJobHandler {
+    @Override
+    public ReturnT<String> execute(String s) {
+        return SUCCESS;
+    }
+}
