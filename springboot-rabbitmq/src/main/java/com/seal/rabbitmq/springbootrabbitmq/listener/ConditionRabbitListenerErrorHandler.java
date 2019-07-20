d7938a008cff0e9f32bc6a1d38886b77815c5e99
youngMen1 <fengzhiqiangzxx199644>
2019-07-20 18:07:01 +0800

    新增rabbitmq

--- springboot-rabbitmq/src/main/java/com/seal/rabbitmq/springbootrabbitmq/listener/ConditionRabbitListenerErrorHandler.java
+++ springboot-rabbitmq/src/main/java/com/seal/rabbitmq/springbootrabbitmq/listener/ConditionRabbitListenerErrorHandler.java
@@ -1,4 +1,4 @@
-package com.seal.rabbitmq.springbootrabbitmq.Listener;
+package com.seal.rabbitmq.springbootrabbitmq.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
