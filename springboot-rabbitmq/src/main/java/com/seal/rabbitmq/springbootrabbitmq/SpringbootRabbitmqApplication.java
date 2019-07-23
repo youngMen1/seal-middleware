1a61e8268964e153da508887dee79b6c3931958d
youngMen1 <fengzhiqiangzxx199644>
2019-07-23 11:24:17 +0800

    修改名称

--- springboot-rabbitmq/src/main/java/com/seal/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ springboot-rabbitmq/src/main/java/com/seal/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author fengzhiqiang
  * @version 1.0
  * @date 2019/6/16 10:52
+ * @description rabbitmq 服务
  * Broker:它提供一种传输服务,它的角色就是维护一条从生产者到消费者的路线，保证数据能按照指定的方式进行传输,
  * Exchange：消息交换机,它指定消息按什么规则,路由到哪个队列。
  * Queue:消息的载体,每个消息都会被投到一个或多个队列。
